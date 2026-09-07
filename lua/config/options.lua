1665646e59e45a88d7d7a3bb7853586830ac5c2c
Ahmed Shahwan <a.shahwan42@gmail.com>
2026-09-07 04:55:54 +0300

    Enable OSC 52 clipboard over remote sessions

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_php_lsp = "intelephense"
+
+-- Herdr forwards OSC 52 writes from remote panes to the local client.
+-- Use it explicitly so regular yanks also reach the desktop clipboard over SSH.
+vim.g.clipboard = "osc52"
+vim.opt.clipboard = "unnamedplus"
